9046e6f3f4df949aacd76c59d28b55f2e408d299
Gleb Pologov <gpologov@gmail.com>
2025-08-07 21:44:39 +0300

    gigachat

--- scripts/ai-pr-review.js
+++ scripts/ai-pr-review.js
@@ -40,7 +40,7 @@ const octokit = new Octokit({
 
 const gigachat = new GigaChat({
     timeout: 600,
-    model: 'GigaChat Max',
+    model: 'GigaChat',
     credentials: GIGACHAT_API_KEY,
     httpsAgent: httpsAgent
 })
